c8e1811d3211b491d1afb3147393100bcc6fd739
Mark Fayngersh <mark@crowdtwist.com>
2013-10-21 18:09:03 -0400

    project.clj format tweaks

--- project.clj
+++ project.clj
@@ -3,15 +3,13 @@
   :test-paths ["test"]
   :aot :all
   :dependencies [
-                   [commons-collections/commons-collections "3.2.1"]
-                 ]
+                  [commons-collections/commons-collections "3.2.1"]
+                ]
 
   :profiles {:dev
               {:dependencies [[storm "0.8.2"]
-                              [org.clojure/clojure "1.4.0"]
                               [org.testng/testng "6.8.5"]
                               [org.easytesting/fest-assert-core "2.0M8"]
                               [org.mockito/mockito-all "1.9.0"]
                               [org.jmock/jmock "2.6.0"]]}}
-  :min-lein-version "2.0.0"
-  )
+  :min-lein-version "2.0.0")
